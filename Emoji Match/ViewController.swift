ce77cb971fe74d95587ad433cc93814ddbaf4d46
Chris Arnoult <chris.arnoult@gmail.com>
2018-02-27 17:48:55 -0800

    Initial Commit

---
+++ Emoji Match/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Match
+//
+//  Created by Chris Arnoult on 2/27/18.
+//  Copyright © 2018 Chris Arnoult. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
